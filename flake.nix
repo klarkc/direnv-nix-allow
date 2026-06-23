c7eb2893716c520c9d8aec302193943b0b4adc24
Walker Leite <walkerleite490@gmail.com>
2026-06-23 15:32:06 -0300

    Run nix fmt

--- flake.nix
+++ flake.nix
@@ -12,63 +12,61 @@
 
   outputs =
     { self, utils, ... }@inputs:
-    utils.apply-systems
-      { inherit inputs; }
-      (
-        { pkgs, system, ... }:
-        let
-          package = pkgs.haskellPackages.developPackage {
-            root = ./.;
-          };
-          treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
-            projectRootFile = "flake.nix";
+    utils.apply-systems { inherit inputs; } (
+      { pkgs, system, ... }:
+      let
+        package = pkgs.haskellPackages.developPackage {
+          root = ./.;
+        };
+        treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
 
-            programs.nixfmt.enable = true;
-            programs.ormolu.enable = true;
-            programs.prettier.enable = true;
-            programs.shfmt.enable = true;
+          programs.nixfmt.enable = true;
+          programs.ormolu.enable = true;
+          programs.prettier.enable = true;
+          programs.shfmt.enable = true;
 
-            settings.formatter.prettier.includes = [
-              "*.md"
-              "*.yml"
-              "*.yaml"
-            ];
+          settings.formatter.prettier.includes = [
+            "*.md"
+            "*.yml"
+            "*.yaml"
+          ];
+        };
+        pre-commit-check = inputs.git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks.treefmt = {
+            enable = true;
+            package = treefmtEval.config.build.wrapper;
           };
-          pre-commit-check = inputs.git-hooks.lib.${system}.run {
-            src = ./.;
-            hooks.treefmt = {
-              enable = true;
-              package = treefmtEval.config.build.wrapper;
-            };
-          };
-        in
-        {
-          packages.default = package;
-          apps.default = {
-            type = "app";
-            program = "${package}/bin/direnv-nix-allow";
-          };
+        };
+      in
+      {
+        packages.default = package;
+        apps.default = {
+          type = "app";
+          program = "${package}/bin/direnv-nix-allow";
+        };
 
-          formatter = treefmtEval.config.build.wrapper;
+        formatter = treefmtEval.config.build.wrapper;
 
-          checks = {
-            inherit package pre-commit-check;
-            formatting = treefmtEval.config.build.check self;
-          };
+        checks = {
+          inherit package pre-commit-check;
+          formatting = treefmtEval.config.build.check self;
+        };
 
-          devShells.default = pkgs.mkShell {
-            inherit (pre-commit-check) shellHook;
-            packages = with pkgs; [
-              cabal-install
-              direnv
-              haskell-language-server
-              hlint
-              treefmtEval.config.build.wrapper
-            ];
-            inputsFrom = [
-              package.env
-            ];
-          };
-        }
-      );
+        devShells.default = pkgs.mkShell {
+          inherit (pre-commit-check) shellHook;
+          packages = with pkgs; [
+            cabal-install
+            direnv
+            haskell-language-server
+            hlint
+            treefmtEval.config.build.wrapper
+          ];
+          inputsFrom = [
+            package.env
+          ];
+        };
+      }
+    );
 }
