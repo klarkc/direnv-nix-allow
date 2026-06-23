669240b46cc1a09a81907eede67a922a0c907bd6
KlarkC <walkerleite490@gmail.com>
2026-06-23 16:24:47 -0300

    Add haskell.nix package set

---
+++ nix/pkgs.nix
@@ -0,0 +1,21 @@
+{ inputs, system }:
+
+import inputs.nixpkgs {
+  inherit system;
+  config = inputs.haskell-nix.config;
+  overlays = [
+    inputs.iohk-nix.overlays.crypto
+    inputs.iohk-nix.overlays.cardano-lib
+    inputs.haskell-nix.overlay
+    inputs.iohk-nix.overlays.haskell-nix-crypto
+    inputs.iohk-nix.overlays.haskell-nix-extra
+    (final: prev: {
+      haskell = prev.haskell // {
+        compiler = prev.haskell.compiler // {
+          ghc943 = prev.haskell.compiler.ghc948;
+          ghc944 = prev.haskell.compiler.ghc948;
+        };
+      };
+    })
+  ];
+}
