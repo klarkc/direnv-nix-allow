ef786794f3d2d45f16c079087907c163835c6698
KlarkC <walkerleite490@gmail.com>
2026-06-23 16:01:58 -0300

    Refactor flake to iogx

--- flake.nix
+++ flake.nix
@@ -1,72 +1,27 @@
 {
   description = "Nix-aware direnv approval reuse";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:ursi/flake-utils";
-    git-hooks.url = "github:cachix/git-hooks.nix";
-    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-  };
+  inputs.iogx.url = "github:input-output-hk/iogx";
+  inputs.nixpkgs.follows = "iogx/nixpkgs";
 
   outputs =
-    { self, utils, ... }@inputs:
-    utils.apply-systems { inherit inputs; } (
-      { pkgs, system, ... }:
-      let
-        package = pkgs.haskellPackages.developPackage {
-          root = ./.;
-        };
-        treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
-          projectRootFile = "flake.nix";
+    { self, iogx, ... }@inputs:
+    iogx.lib.mkFlake {
+      inherit inputs;
+      repoRoot = ./.;
+      outputs = import ./nix/outputs.nix;
+      systems = [ "x86_64-linux" ];
+    };
 
-          programs.nixfmt.enable = true;
-          programs.ormolu.enable = true;
-          programs.prettier.enable = true;
-          programs.shfmt.enable = true;
-
-          settings.formatter.prettier.includes = [
-            "*.md"
-            "*.yml"
-            "*.yaml"
-          ];
-        };
-        pre-commit-check = inputs.git-hooks.lib.${system}.run {
-          src = ./.;
-          hooks.treefmt = {
-            enable = true;
-            package = treefmtEval.config.build.wrapper;
-          };
-        };
-      in
-      {
-        packages.default = package;
-        apps.default = {
-          type = "app";
-          program = "${package}/bin/direnv-nix-allow";
-        };
-
-        formatter = treefmtEval.config.build.wrapper;
-
-        checks = {
-          inherit package pre-commit-check;
-          formatting = treefmtEval.config.build.check self;
-        };
-
-        devShells.default = pkgs.mkShell {
-          inherit (pre-commit-check) shellHook;
-          packages = with pkgs; [
-            cabal-install
-            direnv
-            haskell-language-server
-            hlint
-            treefmtEval.config.build.wrapper
-          ];
-          inputsFrom = [
-            package.env
-          ];
-        };
-      }
-    );
+  nixConfig = {
+    accept-flake-config = true;
+    extra-experimental-features = "nix-command flakes";
+    allow-import-from-derivation = "true";
+    extra-substituters = [
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+  };
 }
