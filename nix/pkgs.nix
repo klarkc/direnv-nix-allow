0debe7254a8cbef65d76a07dd9cd16e6460cb000
KlarkC <walkerleite490@gmail.com>
2026-06-23 16:49:19 -0300

    Remove Cardano overlay

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -5,7 +5,6 @@ import inputs.nixpkgs {
   config = inputs.haskell-nix.config;
   overlays = [
     inputs.iohk-nix.overlays.crypto
-    inputs.iohk-nix.overlays.cardano-lib
     inputs.haskell-nix.overlay
     inputs.iohk-nix.overlays.haskell-nix-crypto
     inputs.iohk-nix.overlays.haskell-nix-extra
