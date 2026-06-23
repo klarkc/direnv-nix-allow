10ead8ca8cfb454d6a8d946b5eccb92bed151ac5
KlarkC <walkerleite490@gmail.com>
2026-06-23 16:23:10 -0300

    Use current iogx Haskell template inputs

--- flake.nix
+++ flake.nix
@@ -1,30 +1,52 @@
 {
   description = "Nix-aware direnv approval reuse";
 
-  inputs.iogx.url = "github:input-output-hk/iogx";
-  inputs.nixpkgs.follows = "iogx/nixpkgs";
+  inputs = {
+    iogx.url = "github:input-output-hk/iogx";
 
-  outputs =
-    { self, iogx, ... }@inputs:
-    iogx.lib.mkFlake {
-      inherit inputs;
-      repoRoot = ./.;
-      outputs = import ./nix/outputs.nix;
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
+    haskell-nix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.hackage.follows = "hackage";
     };
+
+    nixpkgs.follows = "haskell-nix/nixpkgs";
+
+    hackage = {
+      url = "github:input-output-hk/hackage.nix";
+      flake = false;
+    };
+
+    CHaP = {
+      url = "github:IntersectMBO/cardano-haskell-packages?ref=repo";
+      flake = false;
+    };
+
+    iohk-nix = {
+      url = "github:input-output-hk/iohk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "aarch64-linux"
+    ] (system: import ./nix/outputs.nix { inherit inputs system; });
 
   nixConfig = {
     accept-flake-config = true;
     extra-experimental-features = "nix-command flakes";
-    allow-import-from-derivation = "true";
+    allow-import-from-derivation = true;
     extra-substituters = [
       "https://cache.iog.io"
+      "https://cache.zw3rk.com"
     ];
     extra-trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
     ];
   };
 }
